ea4d6d2a5b191252a9c824e11cecfca1c81fe88f
David Edelstein <yitzedelstein@gmail.com>
2019-02-11 16:20:52 -0500

    new branch with cSharp HTTPcall for weather api

--- CoffeeShopApp2.0/CoffeeShopApp2.0/Controllers/HomeController.cs
+++ CoffeeShopApp2.0/CoffeeShopApp2.0/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CoffeeShopApp2._0.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,8 +11,22 @@ namespace CoffeeShopApp2._0.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string lat = "", string lon= "")
         {
+            if (!string.IsNullOrEmpty(lat) && !string.IsNullOrEmpty(lon))
+            {
+                using (var client = new HttpClient())
+                {
+                    var a = $"http://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}&FcstType=json";
+                    var b = client.GetAsync(a).ConfigureAwait(false).GetAwaiter().GetResult();
+                    var c = b.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+
+                    var jsonString = client.GetStringAsync($"https://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}&FcstType=json").ConfigureAwait(false).GetAwaiter().GetResult();
+                    var model = JsonConvert.DeserializeObject<WeeklyForecast>(jsonString);
+                    return View(model);
+                }
+            }
+
             return View();
         }
 
---
+++ CoffeeShopApp2.0/CoffeeShopApp2.0/Models/WeeklyForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShopApp2._0.Models
+{
+    public class WeeklyForecast
+    {
+        public WeeklyForecastData Data { get; set; }
+    }
+
+    public class WeeklyForecastData
+    {
+        public string[] Temperature { get; set; }
+    }
+}
